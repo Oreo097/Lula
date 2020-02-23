8dbd9397753a1fc5e593210ed4ef13daa366f12e
Oreo097 <ztdeyouxiang@qq.com>
2020-02-23 18:47:28 +0800

    new file:   .vs/Lula/DesignTimeBuild/.dtbcache 	new file:   .vs/Lula/v16/.suo 	new file:   .vs/Lula/v16/Server/sqlite3/db.lock 	new file:   .vs/Lula/v16/Server/sqlite3/storage.ide 	new file:   App.xaml 	new file:   App.xaml.cs 	new file:   AssemblyInfo.cs 	new file:   Lula.csproj 	new file:   Lula.csproj.user 	new file:   Lula.sln 	new file:   MainWindow.xaml 	new file:   MainWindow.xaml.cs 	new file:   bin/Debug/netcoreapp3.1/Lula.deps.json 	new file:   bin/Debug/netcoreapp3.1/Lula.dll 	new file:   bin/Debug/netcoreapp3.1/Lula.exe 	new file:   bin/Debug/netcoreapp3.1/Lula.pdb 	new file:   bin/Debug/netcoreapp3.1/Lula.runtimeconfig.dev.json 	new file:   bin/Debug/netcoreapp3.1/Lula.runtimeconfig.json 	new file:   obj/Debug/netcoreapp3.1/App.g.cs 	new file:   obj/Debug/netcoreapp3.1/App.g.i.cs 	new file:   obj/Debug/netcoreapp3.1/Lula.AssemblyInfo.cs 	new file:   obj/Debug/netcoreapp3.1/Lula.AssemblyInfoInputs.cache 	new file:   obj/Debug/netcoreapp3.1/Lula.assets.cache 	new file:   obj/Debug/netcoreapp3.1/Lula.csproj.FileListAbsolute.txt 	new file:   obj/Debug/netcoreapp3.1/Lula.csprojAssemblyReference.cache 	new file:   obj/Debug/netcoreapp3.1/Lula.designer.deps.json 	new file:   obj/Debug/netcoreapp3.1/Lula.designer.runtimeconfig.json 	new file:   obj/Debug/netcoreapp3.1/Lula.dll 	new file:   obj/Debug/netcoreapp3.1/Lula.exe 	new file:   obj/Debug/netcoreapp3.1/Lula.g.resources 	new file:   obj/Debug/netcoreapp3.1/Lula.pdb 	new file:   obj/Debug/netcoreapp3.1/Lula_MarkupCompile.cache 	new file:   obj/Debug/netcoreapp3.1/Lula_MarkupCompile.i.cache 	new file:   obj/Debug/netcoreapp3.1/Lula_MarkupCompile.i.lref 	new file:   obj/Debug/netcoreapp3.1/Lula_MarkupCompile.lref 	new file:   obj/Debug/netcoreapp3.1/MainWindow.baml 	new file:   obj/Debug/netcoreapp3.1/MainWindow.g.cs 	new file:   obj/Debug/netcoreapp3.1/MainWindow.g.i.cs 	new file:   obj/Lula.csproj.nuget.cache 	new file:   obj/Lula.csproj.nuget.dgspec.json 	new file:   obj/Lula.csproj.nuget.g.props 	new file:   obj/Lula.csproj.nuget.g.targets 	new file:   obj/project.assets.json

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace Lula
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+   
+    public partial class MainWindow : Window
+    {
+        private static Encoding encode = Encoding.Default;
+        IPAddress ipAddress;
+        Socket mysocket;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ipAddress = IPAddress.Parse("192.168.0.107");
+            mysocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        private void UserPasswdInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Login_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (UserIDInput.Text == null || UserIDInput.Text == "")
+            {
+                Tip_label.Content = "请输入用户ID";
+            }else if (UserPasswdInput.Text == null || UserPasswdInput.Text == "")
+            {
+                Tip_label.Content = "请输入密码";
+            }
+            else
+            {
+                String User_id = UserIDInput.Text;
+                String User_passwd = UserPasswdInput.Text;
+                try
+                {
+                    mysocket.Connect(new IPEndPoint(ipAddress, 1900)); //配置服务器IP与端口
+                    Tip_label.Content = ("连接服务器成功");
+                    mysocket.Send(encode.GetBytes("-"+User_passwd+"-"+User_id+"\n"));
+                }
+                catch
+                {
+                    Tip_label.Content = "链接错误";
+                }
+
+            }
+            
+            
+        }
+    }
+}
